1e315079b17b640e6940705211e7243020585342
khyatikaa-svg <khyati2421@gmail.com>
2025-11-26 23:49:50 +0530

    Add files via upload

---
+++ Question27.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= 2*n; i += 2) {
+        sum += i;   // add odd numbers
+    }
+
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+    return 0;
+}
